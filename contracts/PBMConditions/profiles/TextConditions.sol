ceb8c2791cd9a9402370e9ec8e09754ecb60f5f6
RICHARD NG <richardnky@hotmail.com>
2023-10-08 19:14:39 +0800

    updated textconditions

--- contracts/PBMConditions/profiles/TextConditions.sol
+++ contracts/PBMConditions/profiles/TextConditions.sol
@@ -1,24 +1,42 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-abstract contract TextConditions is Ownable {
-    mapping(string => string[]) private textConditions;
 
-    modifier checkDynamicConditions(string memory key, string memory value) {
-        require(isTextConditionMet(key, value), "Text condition not met");
-        _;
+abstract contract TextConditions {
+    mapping(string => string[]) public conditions;
+    string[] public conditionKeys;
+
+    // Function to add a new condition and update the keys array
+    function addCondition(string memory key, string[] memory values) public  {
+        require(conditions[key].length == 0, "Condition key already exists");
+        conditions[key] = values;
+        conditionKeys.push(key);
     }
 
-    // Function to set a text condition
-    function setTextCondition(string memory key, string[] memory values) public onlyOwner {
-        textConditions[key] = values;
+    // Function to update an existing condition
+    function updateCondition(string memory key, string[] memory values) public {
+        require(conditions[key].length > 0, "Condition key does not exist");
+        conditions[key] = values;
+    }
+
+    // Function to delete an existing condition and remove it from the keys array
+    function deleteCondition(string memory key) public {
+        require(conditions[key].length > 0, "Condition key does not exist");
+        delete conditions[key];
+        for (uint256 i = 0; i < conditionKeys.length; i++) {
+            if (keccak256(abi.encodePacked(conditionKeys[i])) == keccak256(abi.encodePacked(key))) {
+                if (i != conditionKeys.length - 1) {
+                    conditionKeys[i] = conditionKeys[conditionKeys.length - 1];
+                }
+                conditionKeys.pop();
+                break;
+            }
+        }
     }
 
     // Function to check if a text condition is met
-    // It will check a single "value" against an array of values
-    function isTextConditionMet(string memory key, string memory value) internal view returns (bool) {
-        string[] memory allowedValues = textConditions[key];
+    function isTextConditionMet(string memory key, string memory value) public view returns (bool) {
+        string[] memory allowedValues = conditions[key];
         for (uint256 i = 0; i < allowedValues.length; i++) {
             if (keccak256(abi.encodePacked(allowedValues[i])) == keccak256(abi.encodePacked(value))) {
                 return true;
@@ -27,9 +45,13 @@ abstract contract TextConditions is Ownable {
         return false;
     }
 
-    // // Function that uses the checkDynamicConditions modifier
-    // function someFunction(string memory key, string memory value) public checkDynamicConditions(key, value) {
-    //     // Your code here
-    // }
+    // Function to get all condition keys
+    function getAllConditionKeys() public view returns (string[] memory) {
+        return conditionKeys;
+    }
 
+    // Function to get all conditions for a specific key
+    function getAllConditionsForKey(string memory key) public view returns (string[] memory) {
+        return conditions[key];
+    }
 }
